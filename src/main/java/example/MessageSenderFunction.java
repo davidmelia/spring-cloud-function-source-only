c1902f82c6f8c0a2d32463a130c12477355c9190
meliad2 <david.melia@ii.co.uk>
2022-11-09 10:33:44 +0000

    better

--- src/main/java/example/MessageSenderFunction.java
+++ src/main/java/example/MessageSenderFunction.java
@@ -1,18 +1,17 @@
 package example;
 
+import java.time.Duration;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
-
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -21,23 +20,22 @@ import reactor.core.publisher.Mono;
 @AllArgsConstructor
 public class MessageSenderFunction implements Function<Flux<Map<String, String>>, Flux<String>> {
 
-	private StreamBridge streamBridge;
-	// private final MessageChannelPublisher messageChannelPublisher;
+  private StreamBridge streamBridge;
 
-	@Override
-	public Flux<String> apply(Flux<Map<String, String>> flux) {
-		return flux.flatMap(v -> {
-			Object kafkaEvent = MessageBuilder
-					.withPayload(Map.of("Key", String.format("Value @ %s", ZonedDateTime.now(ZoneOffset.UTC))))
-					.setHeaderIfAbsent(KafkaHeaders.KEY, UUID.randomUUID().toString()).build();
-			log.info("Sending message to binding = {}", kafkaEvent);
-			if (streamBridge.send("test-out-0", kafkaEvent)) {
-				log.info("Message sent to binding = {}", kafkaEvent);
-				return Mono.just("OK");
-			} else {
-				log.error("Error occurred while sending message = {} to the binding.", kafkaEvent);
-				return Mono.error(new RuntimeException("event publishing failed"));
-			}
-		});
-	}
+  @Override
+  public Flux<String> apply(Flux<Map<String, String>> flux) {
+    return flux.flatMap(v -> {
+      Object kafkaEvent = MessageBuilder
+          .withPayload(Map.of("Key", String.format("Value @ %s", ZonedDateTime.now(ZoneOffset.UTC))))
+          .setHeaderIfAbsent(KafkaHeaders.KEY, UUID.randomUUID().toString()).build();
+      log.info("Sending message to binding = {}", kafkaEvent);
+      if (streamBridge.send("test-out-0", kafkaEvent)) {
+        log.info("Message sent to binding = {}", kafkaEvent);
+        return Mono.just("OK").delayElement(Duration.ofSeconds(1));
+      } else {
+        log.error("Error occurred while sending message = {} to the binding.", kafkaEvent);
+        return Mono.error(new RuntimeException("event publishing failed"));
+      }
+    });
+  }
 }
--- src/test/java/example/CallFunctionInvoker.java
+++ src/test/java/example/CallFunctionInvoker.java
@@ -2,56 +2,28 @@ package example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Map;
-
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CallFunctionInvoker {
 
-	/**
-	 * results in function-out-0=UNKNOWN_TOPIC_OR_PARTITION
-	 * 
-	 * <pre>
-	 * 2022-10-25 11:13:16.089  WARN 68925 --- [ad | producer-1] org.apache.kafka.clients.NetworkClient   : [Producer clientId=producer-1] Error while fetching metadata with correlation id 33 : {function-out-0=UNKNOWN_TOPIC_OR_PARTITION}
-	 * </pre>
-	 */
-	@Test
-	public void runFunctionInvoker_UNKNOWN_TOPIC_OR_PARTITION() throws IOException {
-		System.setProperty("spring.cloud.function.definition", "function");
-		FunctionInvoker handler = new FunctionInvoker();
-		ObjectMapper om = new ObjectMapper();
-		ByteArrayInputStream is = new ByteArrayInputStream(om.writeValueAsBytes(Map.of()));
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		handler.handleRequest(is, outputStream, null);
-		log.info("the result is {}", outputStream.toString());
-		assertEquals("\"dave\"", outputStream.toString());
-	}
-
-	/**
-	 * Failed to establish route
-	 * 
-	 * <pre>
-	 * java.lang.IllegalStateException: Failed to establish route, since neither were provided: 'spring.cloud.function.definition' as Message header or as application property or 'spring.cloud.function.routing-expression' as application property. Incoming message: GenericMessage [payload=byte[2], headers={id=0b099ec3-480d-6d92-077c-e42d31dd3dc9, timestamp=1666692939591}]
-	 * </pre>
-	 */
-	@Test
-	public void runFunctionInvoker_Failed_to_establish_route() throws IOException {
-		FunctionInvoker handler = new FunctionInvoker();
-		ObjectMapper om = new ObjectMapper();
-		ByteArrayInputStream is = new ByteArrayInputStream(om.writeValueAsBytes(Map.of()));
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		handler.handleRequest(is, outputStream, null);
-		log.info("the result is {}", outputStream.toString());
-		assertEquals("\"dave\"", outputStream.toString());
-	}
+  @Test
+  public void runFunctionInvoker() throws IOException {
+    System.setProperty("MAIN_CLASS", FunctionApplication.class.getCanonicalName());
+    FunctionInvoker handler = new FunctionInvoker("function");
+    ObjectMapper om = new ObjectMapper();
+    ByteArrayInputStream is = new ByteArrayInputStream(om.writeValueAsBytes(Map.of()));
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    handler.handleRequest(is, outputStream, null);
+    log.info("the result is {}", outputStream.toString());
+    assertEquals("\"OK\"", outputStream.toString());
+  }
 
 }
